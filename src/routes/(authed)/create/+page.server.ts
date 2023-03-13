c582922c8b4cbeb9a8c2070167bf1082b736e206
Zeu Capua <zeucapua@gmail.com>
2023-03-13 00:06:38 -0700

    init restructure create form

--- src/routes/(authed)/create/+page.server.ts
+++ src/routes/(authed)/create/+page.server.ts
@@ -1,72 +1,79 @@
 import { prisma } from "$lib/prisma";
-import slugify from "@sindresorhus/slugify";
 import { fail, redirect } from "@sveltejs/kit";
 import type { Actions } from "@sveltejs/kit";
+import slugify from "@sindresorhus/slugify";
+import { onMount } from "svelte";
 
 export const actions : Actions = {
   findClub: async ({ cookies, request }) => {
     const data = await request.formData();
-    const club_input = data.get("club_name") as string;
+    const club_name = data.get("club_name") as string;
     const auth_id = cookies.get("auth_id");
 
-    const club_name = slugify(club_input);
-
-    const response = await prisma.club.findUnique({
-      where: { name: club_name },
+    let club_slug = slugify(club_name); 
+    let response = await prisma.club.findUnique({
+      where: { slug: club_slug },
       include: { advisors: true }
     });
 
     if (response) {
-      const exisiting_advisors = response.advisors;
-      for (const a of exisiting_advisors) {
-        if (a.auth_id === auth_id) { return { status: "presently" }; }
+      let slug_variation = club_slug + Math.floor(Math.random() * 999);
+      let valid = false;
+      do {
+        let response = await prisma.club.findUnique({
+          where: { slug: slug_variation }
+        });
+        valid = !response;
+      } while (!valid);
+
+      const existing_advisors = response.advisors;
+      for (const a of existing_advisors) {
+        if (a.auth_id === auth_id) { 
+          return { club_name, club_slug, slug_variation, status: "presently", advisors: existing_advisors }
+        }
       }
-      return { club_name, status: "found", advisors: exisiting_advisors}; 
+
+      return { club_name, club_slug, slug_variation, status: "found", advisors: existing_advisors }; 
     }
-    return { club_name, status: "available" }
+    
+    return { club_name, club_slug, status: "available" }
   },
   joinClub: async ({ cookies, request }) => {
     const data = await request.formData();
-    const club_input = data.get("club_name") as string;
+    const club_name = data.get("club_name") as string;
+    const club_slug = data.get("club_slug") as string;
+    const slug_variation = data.get("slug_variation") as string;
+    const final_slug = data.get("final_slug") as string;
+
     const auth_id = cookies.get("auth_id") as string;
-    const status = data.get("status") as string ;
+    const status = data.get("status") as string;
+
     const secret = data.get("secret") as string;
-    const secret2 = data.get("secret2") as string;
-
-    const club_name = slugify(club_input);
-
-    if (secret != secret2) {
-      return fail(400, { club_name, status, unmatching: true });
+    const confirm_secret = data.get("secret2") as string;
+    
+    if (secret != confirm_secret) {
+      return fail(400, { club_name, club_slug, slug_variation, final_slug, status, unmatched: true }); 
     }
 
+  },
+  createClub: async ({ cookies, request }) => {
+    const data = await request.formData();
+    const club_name = data.get("club_name") as string;
+    const club_slug = data.get("club_slug") as string;
 
-    if (status === "found") {
-      const response = await prisma.club.findUnique({
-        where: { name: club_name }
-      });
-      if (response && response.secret === secret) {
-        const response = await prisma.club.update({
-          where: { name: club_name },
-          data: {
-            advisors: { connect: { auth_id } }
-          }
-        });
-        throw redirect(303, "/dashboard");
-      }
-      else {
-        return { status: "fail" }
-      }
-    }
+    const auth_id = cookies.get("auth_id") as string;
+    const status = data.get("status") as string;
 
-    const response = await prisma.club.create({
+    const secret = data.get("secret") as string;
+    const confirm_secret = data.get("secret2") as string;
+
+    await prisma.club.create({
       data: {
         name: club_name,
+        slug: club_slug,
         secret: secret,
         advisors: { connect: { auth_id } }
       }
     });
-
-    throw redirect(303, "/dashboard");
-
-  }
+  },
 }
--- src/routes/(authed)/create/+page.svelte
+++ src/routes/(authed)/create/+page.svelte
@@ -8,6 +8,9 @@
   let value : string | null;
   export let form : ActionData;
 
+  let action : string;
+
+
   onMount(() => {
     const searchParams = $page.url.searchParams;
     value = searchParams.get("value");
@@ -17,49 +20,71 @@
   $: console.log({form});
 </script>
 
+<main class="flex flex-col w-full min-w-screen h-full min-h-screen p-16 gap-8">
+
+{#if value === "club"}
 
 <form method="POST" use:enhance>
-{#if !form || form?.status === "presently"}
+  <fieldset class="w-full max-w-3xl h-full gap-4">
+    <legend class="text-5xl font-comiccat">Lead a Club!</legend>
+    <input name="club_name" type="text" value={form?.club_name ?? ""}/>
+    <button class="border-2 border-dotted hover:border-solid font-comiccat">Find</button>
+  </fieldset>
 
-  {#if form?.status === "presently"}
-    <p>You are already in this club!</p>
+  {#if form}
+
+    {#if form.status === "presently"}
+      <p>You are already in lophop.club/{form.club_slug}</p>
+    {:else if form.status === "found"}
+      <label>
+        <input type="radio" bind:group={action} value="join">
+        <p>Club already exists at </p>
+        <p>lophop.club/{form.club_slug}</p>
+      </label>
+    {/if}
+
+    <label>
+      <input type="radio" bind:group={action} value="create">
+      <p>Create a new club at </p>
+      <p>lophop.club/{form.slug_variation}</p>
+    </label>
+
+    <input name="final_slug" type="hidden"
+      value={ () => {
+          if (action === "join") { return form?.club_slug; }
+          else if (action === "create") { return form?.slug_variation; }
+          else { return "" }
+      }}
+    />
+
+    {#key action}
+      <p>Enter the Club's Secret</p>
+      {#if form.unmatched}
+        <p>Secrets must be the same!</p> 
+      {/if}
+      <label>
+        Secret
+        <input name="secret" type="password" required />
+      </label>
+      <label>
+        Confirm Secret
+        <input name="confirming_secret" type="password" required />
+      </label>
+      {#if action === "join"}
+        <button formAction="?/joinClub">Join</button>
+      {/if}
+      {#if action === "create"}
+        <button formAction="?/createClub">Join</button>
+      {/if}
+    {/key}
   {/if}
-  <label>
-    Club Name: <input name="club_name" type="text" />
-  </label>
-  <button formAction="?/findClub">Next</button>
-
-{:else} 
-
-
-  {#if form.status === "found"}
-    <p>{form.club_name} found! Enter the secret to join!</p>
-    <p>Other Advisors in this Club:</p>
-    {#each form.advisors as advisor}
-      <p>{advisor.name}</p>
-    {/each}
-  {:else if form.status === "available"}
-    <p>{form.club_name} is available! Set a password to remember</p>
-  {/if}
-
-  {#if form?.unmatching}
-    <p>Secrets must be the same!</p>
-  {/if}
-
-  <label>
-    Secret: <input name="secret" type="password" />
-  </label>
-  <label>
-    Reenter Secret: <input name="secret2" type="password" />
-  </label>
-  <input name="club_name" type="hidden" value={form.club_name} />
-  <input name="status" type="hidden" value={form.status} />
-  <button formAction="?/joinClub">Next</button>
-
-{/if}
 </form>
 
 
+{/if}
 
+
+
+</main>
 
 
