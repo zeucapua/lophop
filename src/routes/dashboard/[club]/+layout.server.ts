3abc2772890708d47a3e053522f83643547571c9
Zeu Capua <zeucapua@gmail.com>
2023-04-23 16:19:57 -0700

    init dashboard/projects page

---
+++ src/routes/dashboard/[club]/+layout.server.ts
@@ -0,0 +1,20 @@
+import { prisma } from "$lib/prisma";
+import { error } from "@sveltejs/kit";
+
+export async function load({ params }) {
+  const club_slug = params.club;
+
+  const club = await prisma.club.findUnique({
+    where: { slug: club_slug },
+    select: {
+      name: true,
+      members: true,
+      advisors: true,
+      slug: true,
+    }
+  });
+
+  if (!club) { throw error(404, "Club not found"); }
+
+  return { club };
+}
--- src/routes/dashboard/[club]/+page.server.ts
+++ src/routes/dashboard/[club]/+page.server.ts
@@ -1,23 +1,6 @@
 import { prisma } from "$lib/prisma";
 import { error } from "@sveltejs/kit";
 
-export async function load({ params }) {
-  const club_slug = params.club;
-
-  const club = await prisma.club.findUnique({
-    where: { slug: club_slug },
-    select: {
-      name: true,
-      members: true,
-      advisors: true,
-      slug: true,
-    }
-  });
-
-  if (!club) { throw error(404, "Club not found"); }
-
-  return { club };
-}
 
 export const actions = {
   register: async ({ request }) => {
